acd649aa23b1f3cd721a82139654a6daefe1e796
Motoki Kawakami <mothule.dev+github@gmail.com>
2019-08-23 17:35:39 +0900

    Fix wrong syntax

--- shared/atom.sh
+++ shared/atom.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash -eu
 
 function append_atom_config() {
-  if [[ -e ~/.atom -a -e ~/.atom/keymap.cson ]] ; then
-    if [[ ! -n "`cat ~/.atom/keymap.cson | grep $1`" ]] ; then
-      echo $1 >> ~/.atom/keymap.cson
+  if [[ -e ~/.atom ]] ; then
+    if [[ -e ~/.atom/keymap.cson ]] ; then
+      if [[ ! -n "`cat ~/.atom/keymap.cson | grep $1`" ]] ; then
+        echo $1 >> ~/.atom/keymap.cson
+      fi
     fi
   fi
 }
